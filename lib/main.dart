69a11388cca033bfe5e8ab98600602a059a447c4
emmanuel52 <emmanuelgarciacruz987@gmail.com>
2023-09-27 11:58:15 -0600

    Se quitaron cosas que no son importantes

--- lib/main.dart
+++ lib/main.dart
@@ -20,15 +20,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-//  keytool -genkey -v -keystore C:\Users\emman\AndroidStudioProjects\rutapp/key/rutapp_key.jks -keyalg RSA -keysize 2048 -validity 10000 -alias rutapp
-
-// keytool -list -v -keystore "C:\Users\emman\AndroidStudioProjects\rutapp\key\rutapp_key.jks" -alias rutapp -storepass uptap_password -keypass uptap_password
-
-/*
-  storePassword= uptap_password         E2:E2:73:FE:7B:FF:38:4B:D9:39:66:B7:D8:C1:6D:FF:3F:D3:19:94  com.uptap.rutapp
-keyPassword= uptap_password
-keyAlias= rutapp
-storeFile= C:/Users/emman/AndroidStudioProjects/rutapp/rutapp_key.jks
- */  
