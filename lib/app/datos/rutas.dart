2081cb10983e95c8b91f149f7cde449fab60aa99
emmanuel52 <emmanuelgarciacruz987@gmail.com>
2023-09-30 20:08:58 -0600

    Se agrego la ruta de la Xochimilco

--- lib/app/datos/rutas.dart
+++ lib/app/datos/rutas.dart
@@ -1,15 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-final List<LatLng> routeCoordinates = [
-  LatLng(14.944240, -92.256287),
-  LatLng(14.911874, -92.264891),
+// EMMA
+final List<LatLng> routeCoordinatesXochimilco = [
+  const LatLng(14.944155, -92.256285), //BASE DE IDA
+  const LatLng(14.944114, -92.255507),
+  const LatLng(14.941294, -92.256028),
+  const LatLng(14.941322, -92.256638),
+  const LatLng(14.939444, -92.256647),
+  const LatLng(14.939555, -92.257193),
+  const LatLng(14.937561, -92.257329),
+  const LatLng(14.936803, -92.257424),
+  const LatLng(14.935443, -92.257528),
+  const LatLng(14.934772, -92.257647),
+  const LatLng(14.934829, -92.258060),
+  const LatLng(14.934988, -92.258554),
+  const LatLng(14.934776, -92.259404),
+  const LatLng(14.931353, -92.260494),
+  const LatLng(14.928302, -92.261417),
+  const LatLng(14.925771, -92.262211),
+  const LatLng(14.923465, -92.263020),
+  const LatLng(14.921635, -92.262937),
+  const LatLng(14.917659, -92.263583),
+  const LatLng(14.917302, -92.263547),
+  const LatLng(14.916755, -92.263278),
+  const LatLng(
+      14.916259643111685, -92.26310259671287), // Interseccion en el centro
+  const LatLng(14.914825, -92.264069),
+  const LatLng(14.913462, -92.265027),
+  const LatLng(14.91289719816397, -92.26544257614327),
+  const LatLng(14.912426466069604, -92.26576856711745), // Cam
+  const LatLng(14.911804, -92.264966),
+  const LatLng(14.911874, -92.264891), // BASE DE REGRESO
 ];
 
-final PolylineId polylineXochimilco = PolylineId("route");
-final Polyline polyline = Polyline(
-  polylineId: polylineXochimilco,
+const PolylineId polylineRutaXochimilcoIda = PolylineId("Ruta Xochimilco");
+final Polyline polylineRX = Polyline(
+  polylineId: polylineRutaXochimilcoIda,
   color: Colors.green, // Color de la línea de la ruta
   width: 5, // Ancho de la línea de la ruta
-  points: routeCoordinates,
+  points: routeCoordinatesXochimilco,
 );
+Marker markerIX = const Marker(
+    markerId: MarkerId('marker'),
+    position: LatLng(14.944155, -92.256285),
+    infoWindow: InfoWindow(title: "Base de Ida", snippet: "RUTA XOCHIMILCO"));
+Marker markerRX = const Marker(
+    markerId: MarkerId('marker'),
+    position: LatLng(14.911874, -92.264891),
+    infoWindow:
+        InfoWindow(title: "Base de Regreso", snippet: "RUTA XOCHIMILCO"));
+
+final List<LatLng> regreso = [
+  const LatLng(14.911874, -92.264891),
+  const LatLng(14.912299298147405, -92.26463670968532),
+  const LatLng(14.912867, -92.265454),
+];
+const PolylineId polylineRutaXochimilcoRegreso =
+    PolylineId("Ruta Xochimilco Regreso");
+final Polyline polylineR = Polyline(
+  polylineId: polylineRutaXochimilcoRegreso,
+  color: Colors.blue, // Color de la línea de la ruta
+  width: 5, // Ancho de la línea de la ruta
+  points: regreso,
+);
+
+Set<Polyline> polylineIdaXochi = {polylineRX};
+Set<Polyline> polylineRegreXochi = {polylineR};
+
+Set<Marker> markerXochi = {markerIX, markerRX};
--- lib/app/ui/paginas/home/home_page.dart
+++ lib/app/ui/paginas/home/home_page.dart
@@ -3,10 +3,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:rutapp/app/datos/constantes.dart';
+import 'package:rutapp/app/datos/rutas.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,34 +16,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final Completer<GoogleMapController> _controller = Completer();
-
-  // ignore: constant_identifier_names
-  static const LatLng rutaXochi_Ida = LatLng(14.944240, -92.256287);
-  // ignore: constant_identifier_names
-  static const LatLng rutaXochi_Regre = LatLng(14.911874, -92.264891);
-
-  List<LatLng> dibujarCoordenadas = [];
-
-  void getDibujarRutas() async {
-    PolylinePoints polylinePoints = PolylinePoints();
-    PolylineResult resul = await polylinePoints.getRouteBetweenCoordinates(
-        google_api_key,
-        PointLatLng(rutaXochi_Ida.latitude, rutaXochi_Ida.longitude),
-        PointLatLng(rutaXochi_Regre.latitude, rutaXochi_Regre.longitude));
-
-    if (resul.points.isNotEmpty) {
-      resul.points.forEach(
-        (PointLatLng point) => dibujarCoordenadas.add(
-          LatLng(point.latitude, point.longitude),
-        ),
-      );
-      setState(() {});
-    } else {
-      print("No se encontraron puntos de ruta");
-    }
-  }
-
-  // Set<Polyline> _polylines = {};
 
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(14.90385, -92.25749),
@@ -58,6 +29,7 @@ class _HomePageState extends State<HomePage> {
       zoom: 19.151926040649414);
 
   // ignore: unused_element
+  // NO SE TOCA
   Future<Object> _requestLocationPermission() async {
     LocationPermission permission;
 
@@ -76,7 +48,6 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     // _requestLocationPermission();
     // _addPolyline(); // Agregar la ruta al mapa
-    getDibujarRutas();
     super.initState();
   }
 
@@ -95,20 +66,9 @@ class _HomePageState extends State<HomePage> {
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
-        polylines: {
-          Polyline(
-              polylineId: const PolylineId("ruta"),
-              points: dibujarCoordenadas,
-              color: primaryColor,
-              width: 6),
-        },
-        markers: {
-          const Marker(
-              markerId: MarkerId("XochimilcoIda"), position: rutaXochi_Ida),
-          const Marker(
-              markerId: MarkerId("XochimilcoRegreso"),
-              position: rutaXochi_Regre),
-        },
+        polylines: {...polylineIdaXochi, ...polylineRegreXochi},
+
+        markers: {...markerXochi},
 
         //polylines: //_polylines,
       ),
@@ -128,23 +88,5 @@ class _HomePageState extends State<HomePage> {
 
     await controller
         .animateCamera(CameraUpdate.newCameraPosition(_initialCameraPosition));
-  }
-
-  // ignore: unused_element
-  void _addPolyline() {
-    // ignore: unused_local_variable
-    const Polyline polyline = Polyline(
-      polylineId: PolylineId('RUTA DE LA XOCHIMILCO'),
-      color: Colors.blue,
-      width: 5,
-      points: [
-        LatLng(14.944240, -92.256287),
-        LatLng(14.911874, -92.264891),
-      ],
-    );
-
-    // setState(() {
-    //   _polylines.add(polyline);
-    // });
   }
 }
